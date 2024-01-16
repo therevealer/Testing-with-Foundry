3636f1e7e26ce58595f9df376525c784556026d9
Aguchukwu Emmanuel <officialnfluence@gmail.com>
2024-01-16 23:53:03 +0100

    Formatting

--- src/import.sol
+++ src/import.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "solmate/tokens/ERC20.sol";
+// import "solmate/tokens/ERC20.sol";
 
-contract myToken is ERC20("name", "symbol", 18){}
+// contract myToken is ERC20("name", "symbol", 18){}
